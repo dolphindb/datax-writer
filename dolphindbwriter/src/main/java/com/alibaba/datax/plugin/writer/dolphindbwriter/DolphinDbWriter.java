4b8d06f52386c1cdba9b6b6f2bc9422141b0261b
minxing.zou <minxing.zou@dolphindb.com>
2023-04-20 19:05:19 +0800

    AJ-284: add version 1.30.21.4

--- dolphindbwriter/src/main/java/com/alibaba/datax/plugin/writer/dolphindbwriter/DolphinDbWriter.java
+++ dolphindbwriter/src/main/java/com/alibaba/datax/plugin/writer/dolphindbwriter/DolphinDbWriter.java
@@ -29,6 +29,8 @@ public class DolphinDbWriter extends Writer {
 
     public static class Job extends Writer.Job {
 
+        public static final String VERSION = "1.30.21.4";
+
         private static final Logger LOG = LoggerFactory.getLogger(Job.class);
 
         private Configuration writerConfig = null;
