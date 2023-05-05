7cbeef7e9ec6777e25bff5a494e5593121e4f0b8
chengyitian <sylvestercheng@aliyun.com>
2023-05-05 15:40:52 +0800

    from AJ-285、AJ-286、AJ-288、AJ-289、AJ-290、DPLG-665 to DPLG-1275、DPLG-1276、DPLG-1277、DPLG-1278、DPLG-1279、DPLG-665.

--- dolphindbwriter/src/main/java/com/alibaba/datax/plugin/writer/dolphindbwriter/DolphinDbWriter.java
+++ dolphindbwriter/src/main/java/com/alibaba/datax/plugin/writer/dolphindbwriter/DolphinDbWriter.java
@@ -29,7 +29,7 @@ public class DolphinDbWriter extends Writer {
 
     public static class Job extends Writer.Job {
 
-        public static final String VERSION = "1.30.21.4";
+        public static final String VERSION = "1.30.21.5";
 
         private static final Logger LOG = LoggerFactory.getLogger(Job.class);
 
