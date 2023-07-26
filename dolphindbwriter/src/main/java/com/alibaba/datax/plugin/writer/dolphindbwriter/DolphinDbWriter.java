1a3ccfefa0b2fd8a999b8af66cb39d7198b9dba2
chengyitian <sylvestercheng@aliyun.com>
2023-07-26 17:05:38 +0800

    DPLG-1592: update version to 1.30.22.3.

--- dolphindbwriter/src/main/java/com/alibaba/datax/plugin/writer/dolphindbwriter/DolphinDbWriter.java
+++ dolphindbwriter/src/main/java/com/alibaba/datax/plugin/writer/dolphindbwriter/DolphinDbWriter.java
@@ -32,7 +32,7 @@ public class DolphinDbWriter extends Writer {
 
     public static class Job extends Writer.Job {
 
-        public static final String VERSION = "1.30.21.5";
+        public static final String VERSION = "1.30.22.3";
 
         private static final Logger LOG = LoggerFactory.getLogger(Job.class);
 
