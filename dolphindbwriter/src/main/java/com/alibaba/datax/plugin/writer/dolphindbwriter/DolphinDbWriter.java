1e490a18313d269d7baa31a0ae8db7a818be6a11
chengyitian <sylvestercheng@aliyun.com>
2023-09-06 15:45:48 +0800

    JAVAOS-170: adjust prohect structure.

--- dolphindbwriter/src/main/java/com/alibaba/datax/plugin/writer/dolphindbwriter/DolphinDbWriter.java
+++ dolphindbwriter/src/main/java/com/alibaba/datax/plugin/writer/dolphindbwriter/DolphinDbWriter.java
@@ -30,9 +30,9 @@ import java.util.*;
 
 public class DolphinDbWriter extends Writer {
 
-    public static class Job extends Writer.Job {
+    private static final String DOLPHINDB_DATAX_WRITER_VERSION = "1.30.22.1";
 
-        public static final String VERSION = "1.30.22.1";
+    public static class Job extends Writer.Job {
 
         private static final Logger LOG = LoggerFactory.getLogger(Job.class);
 
